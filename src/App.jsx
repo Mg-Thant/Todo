753bb2a1d00c2d2bbf1398ae0343397f15010bf2
Mg-Thant <tzinwin517@gmail.com>
2024-05-27 16:54:34 +0630

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(false);
+  const [todo, setTodo] = useState(undefined);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const getData = async (e) => {
+    e.preventDefault();
 
-  const fetchData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    if (id < 1) {
+      setError(true);
+      setId("");
+      return;
+    }
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+
     const data = await response.json();
-    setTodos(data);
+    setError(false);
+    setTodo(data);
+    setId("");
   };
 
   return (
-    <section className="App">
-      <table>
-        <thead>
-          <th>
-            <th>id</th>
-            <th>Title</th>
-            <th>Completed</th>
-          </th>
-        </thead>
-        <tbody>
-          {todos.map((todo) => (
-            <tr key={todo.id}>
-              <td>{todo.id}</td>
-              <td>{todo.title}</td>
-              {todo.completed ? <p>Done</p> : <p className="none">None</p>}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <section>
+      <form onSubmit={getData}>
+        <input
+          type="number"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <button type="submit">Get Data</button>
+      </form>
+      <div>
+        {error && <h1>Please enter an vaild id. (example 1, 2, 3, etc ...)</h1>}
+        {todo && (
+          <div>
+            <h1>id - {todo.id}</h1>
+            <h1>title - {todo.title}</h1>
+            <h1>userID - {todo.userId}</h1>
+            <h1>
+              completed -{" "}
+              {todo.completed ? (
+                <span>Completed</span>
+              ) : (
+                <span className="none">Not Completed</span>
+              )}
+            </h1>
+          </div>
+        )}
+      </div>
     </section>
   );
 }
